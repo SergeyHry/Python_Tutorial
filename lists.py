fd03800fd77030539b221905106183bfe362f075
SergeyHry <hryshynde@gmail.com>
2025-10-10 14:44:22 +0200

    classen und Lists
    weiter mit Lists und classen gearbeitet

--- lists.py
+++ lists.py
@@ -10,3 +10,27 @@ print(peopleString.split(", "))
 for i in (people):
     wieVieleBuchstaben = (len("".join(people)))
 print(wieVieleBuchstaben)
+
+def deleteElement(list, string):
+    try:
+        list = [l for l in list if l!= string]
+        print(list)
+    except ValueError as e:
+        print("Value ist Falsch bitte liste und String  eingeben", e)
+    except TypeError as e:
+        print("Der Datentyp ist falsch")
+    finally:
+        print("das Element wurde entfernt.")
+
+
+testList = ["Pepsi", "Fanta", "Cola", "Sprite"]
+deleteElement(testList, "Pepsi")
+try:
+    people = {1: "Anna", 2: "Max", 3: "Tom"}
+    for key in list(people.keys()):
+        if people[key] == "Max":
+            people.pop(key)
+    for v in people.values():
+        print(v +" ", end= "")
+except Exception as e:
+    print("so was geht nicht!", e)
