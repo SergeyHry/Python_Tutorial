cb89f6bc175e5bd70bf9ffbae4f63709050aa784
SergeyHry <hryshynde@gmail.com>
2025-10-10 12:11:32 +0200

    Erstellt eine funktion die Anzahl der buchstaben in einer liste ausgibt

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print ("hey")
+def newFuc(list):
+    for i in (list):
+        zaehlen = (len("".join(list)))
+    print(zaehlen)
+
+newList = ["Banane","Orange", "Zitrone", "Birne"]
+newFuc(newList)
